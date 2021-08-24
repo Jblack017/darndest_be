de77fa6807bb644f1ed99842514df9b1c2972132
Jblack017 <blackhamjoshua@comcast.net>
2021-08-24 10:25:34 -0600

    adds full crud to kids

--- app/controllers/kids_controller.rb
+++ app/controllers/kids_controller.rb
@@ -16,6 +16,17 @@ class KidsController < ApplicationController
     :include => { :comments => {:except => [:created_at, :updated_at, :kid_id]}}
   end
 
+  def update
+    @kid = Kid.find(params[:id])
+
+      if @kid.update(content: params[:content])
+      return render status: :accepted, except: [:created_at, :updated_at]
+    else
+      return render json: {error: @kid.errors.full_messages}
+    end
+    
+  end
+
   def create
     @new_kid = Kid.create!(name: params[:name], age: params[:age])
 
@@ -25,6 +36,19 @@ class KidsController < ApplicationController
       return render json: @new_kid, status: :created, except: [:created_at, :updated_at]
     else
       return render json: {error: @new_kid.errors.full_messages,}
+    end
+
+  end
+
+  def destroy
+    @kid = Kid.find(params[:id])
+
+    if @kid
+      @kid.destroy
+      
+      return render status: :accepted
+    else
+      return render status: :not_found
     end
 
   end
